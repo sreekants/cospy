f60ec86b4a7c069c46f7292525c34c962a3fbb02
Sreekant Sreedharan <sreekant.sreedharan@yahoo.com>
2025-09-22 10:15:43 +0200

    Vector distance calculations

--- src/cos/math/geometry/Position.py
+++ src/cos/math/geometry/Position.py
@@ -2,24 +2,23 @@
 # Filename: Position.py
 # Description: Implementation of the Position class
 
+from cos.math.geometry.Point import Point
 from cos.math.geometry.Vector import Vector
-
 import math
 
-class Position:
+class Position(Point):
 	def __init__(self, x, y):
 		""" Constructor
 		"""
-		self.x  = x
-		self.y  = y
+		Point.__init__(self, x, y)
 		return
-	
+
 	def at(self, v, t: float):
 		"""Linear motion: p(t) = p0 + v * t"""
 		return Vector(self.x, self.y) + v*t
 
-	def dot(self, other) -> float:
-		return self.x*other.x + self.y*other.y
+	def __mul__(self, s: float):
+		return Position(self.x * s, self.y * s)
 
 	def __add__(self, other):
 		return Vector(self.x + other.x, self.y + other.y)
@@ -27,13 +26,6 @@ class Position:
 	def __sub__(self, other):
 		return Vector(self.x - other.x, self.y - other.y)
 
-	def __mul__(self, s: float):
-		return Position(self.x * s, self.y * s)
-
-	def __str__(self):
-		return f'({self.x}, {self.y})'
-
-		
 
 if __name__ == "__main__":
 	test = Position()
--- src/cos/math/geometry/Vector.py
+++ src/cos/math/geometry/Vector.py
@@ -2,7 +2,9 @@
 # Filename: Vector.py
 # Description: Implementation of the vector calculus functions
 
+from cos.math.geometry.Point import Point
 import math
+
 
 class Vector:
     def __init__(self, x, y):
@@ -11,8 +13,28 @@ class Vector:
         self.x  = x
         self.y  = y
         return
-        
-    def dot(self, other) -> float:
+    
+    def to_point(self, p:Point, q:Point):
+        """ Distance from a point to a vector
+        Arguments
+            p -- Point from which to measure distance
+            q -- Point on the vector
+        """ 
+
+		# Refer: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+		# Topic: A vector projection proof
+
+        x0		= p.x
+        y0		= p.y
+        x1		= q.x
+        y1		= q.y
+        a       = self.x
+        b       = self.y
+        c       = -(a*x1) - (b*y1)
+
+        return abs(a*x0-b*y0+c)/math.sqrt(a**2+b**2)
+
+    def dot(self, other):
         return self.x*other.x + self.y*other.y
 
     def norm(self) -> float:
