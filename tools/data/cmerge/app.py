d93c8d13201f04a8fbbad2c1fc555e0b2307959b
Sreekant Sreedharan <sreekant.sreedharan@yahoo.com>
2026-09-25 18:53:36 +0200

    skeleton merge app

---
+++ tools/data/cmerge/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# Filename: app.py
+# Description: Implementation of the singleton CMergeApp application
+
+class CMergeApp:
+	def __init__(self):
+		return
+
+	def run(self):
+		print( "Hello" )
+		return
+		
+		
+
+if __name__ == "__main__":
+    test = Cmerge()
+
